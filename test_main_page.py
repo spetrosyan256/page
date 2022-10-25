88e8c7b9bffed2f1d8315848c1e23d150fe5d095
spetrosyan256 <116346717+spetrosyan256@users.noreply.github.com>
2022-10-25 19:27:04 +0300

    Delete test_main_page.py

--- test_main_page.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-import time
-link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_linok")
-    login_link.click()
